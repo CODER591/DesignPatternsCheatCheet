8038d26e7dfaf9c201a73b6447d382bd6cd50258
CODER591 <mazorchyk@gmail.com>
2021-01-15 00:43:37 +0200

    Enhancing Composite

--- Patterns/StructuralDesignPatterns/Composite/source/component.h
+++ Patterns/StructuralDesignPatterns/Composite/source/component.h
@@ -7,12 +7,13 @@
 /*
  * Declares the interface for objects in the composition.
  * Implements default behavior for the interface common to all classes, as appropriate. (DoSomething)
- * (My op: (optional)) Declares an interface for accessing and managing its child components. 
+ * (My op: (optional)) Declares an interface for accessing and managing its child components.
  * (optional) defines an interface for accessing a component's parent in the recursive structure, and implements it if that's appropriate.
- * 
- * 
+ *
+ *
  * */
 class Composite;
+
 class Component {
 	public:
 		//lets imagine that we have here the whole interface for leafs
@@ -23,14 +24,14 @@ class Component {
 		virtual ~Component () {
 			std::cout<<"Component_destructed"<<std::endl;
 		}
-		virtual void DoSomethingForComponent(Component* component_p) {}	
+		virtual void DoSomethingForComponent(Component* component_p) {}
 		virtual void addElement(Component * component_ptr) {}
         // client should delete this pointer to avoid memory leak
 		virtual void removeElement(Component* component_p) {}
 		virtual Component* getElement( size_t index) { return nullptr; }
 		virtual void DoSomethingWithEveryOne() {}
 		virtual Composite* GetComposite(){ return nullptr;}
-		
+
 };
 
 
--- Patterns/StructuralDesignPatterns/Composite/source/main.cpp
+++ Patterns/StructuralDesignPatterns/Composite/source/main.cpp
@@ -1,32 +1,44 @@
 
-/* 
+/*
  * The Composite implementation
- * 
+ *
  * Intent:
  *   Compose objects into tree structures to represent part-whole hierarchies.
  *   Composite lets clients treat individual objects and compositions of objects uniformly.
- * 
+ *
  * */
 
 #include "composite.h"
 #include "component.h"
 #include "leaf_1.h"
 #include "leaf_2.h"
+/*
+  Intent:
+	   Achieve genuation and working with composite(complex class/object) as with simple component
+*/
+void Client(Component * something) {
+	something->DoSomething();
+}
+
 
 int main() {
 	Composite obj;
 	Component * kek =new LeafFirst;
 	Component * kek_1 =new LeafSecond;
-	
+
 	obj.addElement(kek);
 	obj.addElement(kek_1);
 	obj.DoSomethingForComponent(kek);
 	obj.DoSomethingWithEveryOne();
-	
+
+	Client(&obj);
+	Client(kek);
+	Client(kek_1);
+
 	obj.removeElement(kek_1);
 	obj.removeElement(kek);
 	delete kek;
 	delete kek_1;
-	
+
 	return 0;
 }
