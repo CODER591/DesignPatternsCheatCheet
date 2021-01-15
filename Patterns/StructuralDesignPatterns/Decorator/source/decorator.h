91fe6ba2e63b55b638008019beae06c3221c0a8a
CODER591 <mazorchyk@gmail.com>
2021-01-15 02:11:28 +0200

    Adding commit to the Decorator

--- Patterns/StructuralDesignPatterns/Decorator/source/decorator.h
+++ Patterns/StructuralDesignPatterns/Decorator/source/decorator.h
@@ -3,13 +3,21 @@
 
 #include "component.h"
 
+/*
+
+  Intent:
+	  Similar to the Composite in case that Decorator could be substituted instead of simple Component.
+		So, here is an advantage in using enhanced Component which is a Decorator.
+
+*/
+
 class Decorator: public Component {
 	private:
 	   Component* comp_instance;
 	public:
 	   Decorator() = default;
 	   Decorator (Component* component):comp_instance(component) {}
-	   
+
 	   virtual void DoSomething() override {
 		   std::cout<<"DoSomething in Decorator style. "<<std::endl;
 		   comp_instance->DoSomething();
